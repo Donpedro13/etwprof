dcc19332b63b429d879fa04d3595c8dd5f542db6
Donpedro13 <2521782+Donpedro13@users.noreply.github.com>
2021-09-06 17:19:55 +0200

    Add utility to dump relevant data and statistics from ETL files (to be used for testing etwprof)
    - The program utilizes Microsoft's TraceProcessor library, and hence uses C#
    - The output of the tool is an XML file with an ad-hoc format, that the Python test cases will be able to read and interpret. Currently, the following data is collected:
      - Processes
      - Threads
      - Images
      - Sampled profile events
      - Context switches
    - I'm borderline illiterate in C# so the code quality is not the best, but it works reasonably well
    - It's not used by any test cases yet, that is coming (hopefully) soon in a later commit

---
+++ Sources/etwprof tests/Utilities/TraceInfoDumper/TraceData.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.Windows.EventTracing;
+using Microsoft.Windows.EventTracing.Cpu;
+using Microsoft.Windows.EventTracing.Processes;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TID
+{
+    class TraceData
+    {
+        #region Trace data structs
+        public struct Process
+        {
+            public Process(int id, string imageName)
+            {
+                Id = id;
+                ImageName = imageName;
+            }
+            public int Id { get; }
+            public string ImageName { get; }
+        }
+
+        public struct Image
+        {
+            public Image(string imageName)
+            {
+                ImageName = imageName;
+            }
+            public string ImageName { get; }
+        }
+
+        public struct Thread
+        {
+            public Thread(int id)
+            {
+                Id = id;
+            }
+            public int Id { get; }
+        }
+
+        public struct ContextSwitchStatistic
+        {
+            public ContextSwitchStatistic(Process process, int count)
+            {
+                Process = process;
+                Count = count;
+            }
+            public Process Process { get; }
+            public int Count { get; }
+        }
+
+        public struct SampledProfileStatistic
+        {
+            public SampledProfileStatistic(Process process, int count)
+            {
+                Process = process;
+                Count = count;
+            }
+            public Process Process { get; }
+            public int Count { get; }
+        }
+        #endregion
+
+        public TraceData(string etlPath)
+        {
+            if (!Path.GetExtension(etlPath).Equals(".etl", StringComparison.CurrentCultureIgnoreCase))
+                throw new ArgumentException("Input must be an ETL file");
+
+            EtlPath = etlPath;
+
+            GatherTraceData();
+        }
+
+        private void GatherTraceData()
+        {
+            using (ITraceProcessor trace = TraceProcessor.Create(EtlPath))
+            {
+                IPendingResult<IProcessDataSource> pendingProcessData = trace.UseProcesses();
+                IPendingResult<IThreadDataSource> pendingThreadData = trace.UseThreads();
+                IPendingResult<ICpuSampleDataSource> pendingCpuSampleData = trace.UseCpuSamplingData();
+                IPendingResult<ICpuSchedulingDataSource> pendingCpuSchedulingData = trace.UseCpuSchedulingData();
+
+                trace.Process();
+
+                GatherProcessData(pendingProcessData.Result);
+                GatherThreadData(pendingThreadData.Result);
+                GatherCpuSampleData(pendingCpuSampleData.Result);
+                GatherCpuSchedulingData(pendingCpuSchedulingData.Result);
+            }
+        }
+
+        private void GatherProcessData(IProcessDataSource processDataSource)
+        {
+            foreach (IProcess process in processDataSource.Processes)
+            {
+                Process ownProcess = new Process(process.Id, process.ImageName);
+                Processes.Add(ownProcess);
+
+                foreach (IImage image in process.Images)
+                {
+                    if (!Images.ContainsKey(ownProcess))
+                        Images.Add(ownProcess, new List<Image>());
+
+                    Images[ownProcess].Add (new Image(image.FileName));
+                }
+            }
+        }
+
+        private void GatherThreadData(IThreadDataSource threadDataSource)
+        {
+            foreach (IThread thread in threadDataSource.Threads)
+            {
+                Process process = new Process(thread.Process.Id, thread.Process.ImageName);
+
+                if(!Threads.ContainsKey(process))
+                    Threads.Add(process, new List<Thread>());
+
+                Threads[process].Add(new Thread(thread.Id));
+            }
+        }
+
+        private void GatherCpuSampleData(ICpuSampleDataSource cpuSampleDataSource)
+        {
+            foreach (ICpuSample sample in cpuSampleDataSource.Samples)
+            {
+                Process process = new Process(sample.Process.Id, sample.Process.ImageName);
+                if (!SampledProfileCounts.ContainsKey(process))
+                    SampledProfileCounts.Add(process, 1);
+                else
+                    ++SampledProfileCounts[process];
+            }
+        }
+
+        private void GatherCpuSchedulingData(ICpuSchedulingDataSource cpuSchedulingDataSource)
+        {
+            void ProcessContextSwitch(IProcess etlProcess)
+            {
+                Process process = new Process(etlProcess.Id, etlProcess.ImageName);
+                if (!ContextSwitchCounts.ContainsKey(process))
+                    ContextSwitchCounts.Add(process, 1);
+                else
+                    ++ContextSwitchCounts[process];
+            }
+
+            foreach (IContextSwitch cswitch in cpuSchedulingDataSource.ContextSwitches)
+            {
+                // If we have a cswitch when the same process's thread is switched, do not count that as 2 context switches
+                if (cswitch.SwitchIn.Process == cswitch.SwitchOut.Process && cswitch.SwitchIn.Process != null)
+                {
+                    ProcessContextSwitch(cswitch.SwitchIn.Process);
+                }
+                else
+                {
+                    if (cswitch.SwitchIn.Process != null)
+                        ProcessContextSwitch(cswitch.SwitchIn.Process);
+
+                    if (cswitch.SwitchOut.Process != null)
+                        ProcessContextSwitch(cswitch.SwitchOut.Process);
+                }
+            }
+        }
+
+        public string EtlPath { get; }
+
+        public List<Process> Processes { get; } = new List<Process>();
+        public Dictionary<Process, List<Image>> Images { get; } = new Dictionary<Process, List<Image>>();
+        public Dictionary<Process, List<Thread>> Threads { get; } = new Dictionary<Process, List<Thread>>();
+        public Dictionary<Process, int> SampledProfileCounts { get; } = new Dictionary<Process, int>();
+        public Dictionary<Process, int> ContextSwitchCounts { get; } = new Dictionary<Process, int>();
+    }
+}
---
+++ Sources/etwprof tests/Utilities/TraceInfoDumper/TraceInfoDumper.cs
@@ -0,0 +1,89 @@
+/* This tool is a small utility used in the testing of etwprof. It takes an input ETL file path parameter, extracts 
+ * some data, and dumps that to an an XML file. For processing ETL files it utilizes the TraceProcessor library by
+ * Microsoft (that's why it's * written in C#, and not C++).
+ */
+
+using Microsoft.Windows.EventTracing;
+using Microsoft.Windows.EventTracing.Processes;
+
+using System;
+using System.IO;
+
+namespace TID
+{
+    class TraceInfoDumperApp
+    {
+        static (string input, string output) GetArgs(string[] args)
+        {
+            if (args.Length != 2)
+                throw new ArgumentOutOfRangeException(nameof(args));
+
+            return (input: args[0], output: args[1]);
+        }
+
+        static void FailWithMessage(string message)
+        {
+            Console.WriteLine($"ERROR: {message}!");
+
+            Environment.Exit(-1);
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine($"Usage: TraceInfoDumper.exe [input ETL path] [output XML path]");
+        }
+
+        static bool IsPathValid(string path)
+        {
+            try
+            {
+                Path.GetFullPath(path);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string input = null, output = null;
+            try
+            {
+                (input, output) = GetArgs(args);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                PrintUsage();
+                FailWithMessage("Invalid arguments");
+            }
+
+            // Some best effort validation on arguments
+            if (!File.Exists(input) || !Path.GetExtension(input).Equals(".etl", StringComparison.InvariantCultureIgnoreCase))
+                FailWithMessage("Invalid input file argument");
+
+            if (!IsPathValid(output) || !Path.GetExtension(output).Equals(".xml", StringComparison.InvariantCultureIgnoreCase))
+                FailWithMessage("Invalid output file argument");
+
+            TraceData traceData = null;
+            try
+            {
+                traceData = new TraceData(input);
+            } catch(Exception e)
+            {
+                FailWithMessage ($"Unable to gather data from the ETL file: {e.Message}");
+            }
+
+            try
+            {
+                TraceDataXmlWriter.Write(traceData, output);
+            }
+            catch (Exception e)
+            {
+                FailWithMessage($"Unable write result XML file: {e.Message}");
+            }
+        }
+    }
+}
